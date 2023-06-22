03f4c58cebf74aa27ee8a9d0d9b35add61ec7e8f
TranHung <tph.hung123@gmail.com>
2023-06-22 20:59:28 +0700

    Update chat

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ImageListModule } from './image-list/image-list.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
     imports: [
@@ -12,6 +13,7 @@ import { ImageListModule } from './image-list/image-list.module';
         ConfigModule.forRoot({ isGlobal: true }),
         AuthModule,
         ImageListModule,
+        MessagesModule,
     ],
     controllers: [AppController],
     providers: [AppService],
